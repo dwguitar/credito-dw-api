fd5a9f7befb16ab7fc27e3687ab4ce018638c26a
DAVINOMARIANO <davino.mariano@pe.senai.br>
2025-06-16 21:40:49 -0300

    Desafio técnico - API de consulta de créditos

---
+++ credito-dw-backend/src/main/java/com/dw/credito/CreditoDwApplication.java
@@ -0,0 +1,13 @@
+package com.dw.credito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditoDwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreditoDwApplication.class, args);
+	}
+
+}
---
+++ credito-dw-backend/src/test/java/com/dw/credito/CreditoDwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dw.credito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditoDwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
